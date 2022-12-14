6eef97bd90924a8a5e19b3efd68b61958f71b65d
code1218 <skjil1218@gmail.com>
2022-12-14 21:49:52 +0900

    데이터베이스 DML 기초

---
+++ select1.sql
@@ -0,0 +1,53 @@
+select 
+	id, 
+	`name`, 
+    age 
+from 
+	student_study_mst 
+where 
+	`name` like '%경%'
+or	`name` like '김%';
+
+select
+	*
+from
+	student_study_mst
+where
+	age > 25
+and age < 30;
+
+select
+	*
+from
+	student_study_mst
+where
+	age >= 25
+and age <= 30;
+
+select
+	*
+from
+	student_study_mst
+where
+	age between 25 and 30;
+    
+select
+	*
+from
+	student_study_mst
+where
+	age not between 25 and 30;
+    
+select
+	*
+from
+	student_study_mst
+where
+	not age = 25;
+    
+select
+	*
+from
+	student_study_mst
+where
+	age is not null;
